d80f59cbfb5dc226963ad26d98c785cdba5e5f57
Usman-ramzan12 <usman.ramzan@wazztech.com>
2022-12-01 14:04:07 +0500

    first commit

---
+++ resources/pos/src/locales/index.js
@@ -0,0 +1,10 @@
+import ar from "./ar.json"; // Arabic
+import en from "./en.json"; // English
+import fr from "./fr.json"; // French
+import gr from "./gr.json"; // German
+import sp from "./sp.json"; // Spanish
+import tr from "./tr.json"; // Turkish
+import vi from "./vi.json"  // Vietnamese
+import cn from "./cn.json"  // Chinese
+
+export default { ar, en, fr, gr, sp, tr, vi, cn };
