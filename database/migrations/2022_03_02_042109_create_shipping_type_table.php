526150a5b0c3e279d3c401b4c4663f4f3b905861
amitjamipara <amitjamipara@gmail.com>
2022-12-19 13:33:21 +0530

    chnage permisson name

--- database/migrations/2022_03_02_042109_create_shipping_type_table.php
+++ database/migrations/2022_03_02_042109_create_shipping_type_table.php
@@ -20,7 +20,7 @@ class CreateShippingTypeTable extends Migration
             $table->timestamps();
         });
         $permissions = array(
-            array('name' =>'manage_shipping_type-index', 's_name' =>'R', 'guard_name' =>'web', 'display_name' => 'Shipping Type View','created_at' => '2022-11-22 05:14:09','updated_at' =>'2022-11-22 05:14:09'),
+            array('name' =>'manage_shipping_type', 's_name' =>'R', 'guard_name' =>'web', 'display_name' => 'Shipping Type View','created_at' => '2022-11-22 05:14:09','updated_at' =>'2022-11-22 05:14:09'),
             array('name' =>'manage_shipping_type-create', 's_name' =>'C', 'guard_name' =>'web', 'display_name' =>'Shipping Type Create','created_at' => '2022-11-22 05:14:09','updated_at' =>'2022-11-22 05:14:09'),
             array('name' =>'manage_shipping_type-edit', 's_name' =>'W', 'guard_name' =>'web', 'display_name' => 'Shipping Type Edit','created_at' => '2022-11-22 05:14:09','updated_at' =>'2022-11-22 05:14:09'),
             array('name' =>'manage_shipping_type-delete', 's_name' =>'D', 'guard_name' =>'web', 'display_name' =>'Shipping Type Delete','created_at' => '2022-11-22 05:14:09','updated_at' =>'2022-11-22 05:14:09'),
