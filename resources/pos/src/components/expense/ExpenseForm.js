f5c8b17db6a3b60471b76c4be5ae49d6287bad77
Usman-ramzan12 <usman.ramzan@wazztech.com>
2022-12-26 17:10:52 +0500

    payment method dropdown added on expense screen

--- resources/pos/src/components/expense/ExpenseForm.js
+++ resources/pos/src/components/expense/ExpenseForm.js
@@ -4,14 +4,18 @@ import {connect} from 'react-redux';
 import {useNavigate} from 'react-router-dom';
 import moment from 'moment';
 import {InputGroup} from 'react-bootstrap-v5';
-import {getFormattedMessage, numValidate, placeholderText} from '../../shared/sharedMethod';
+import {getFormattedMessage, numValidate, placeholderText, getFormattedOptions} from '../../shared/sharedMethod';
 import {editExpense} from '../../store/action/expenseAction';
 import ModelFooter from '../../shared/components/modelFooter';
 import ReactSelect from '../../shared/select/reactSelect';
 import ReactDatePicker from '../../shared/datepicker/ReactDatePicker';
+import {paymentMethodOptions, salePaymentStatusOptions, saleStatusOptions, statusOptions, toastType} from '../../constants';
+
 
 const ExpenseForm = (props) => {
     const {addExpenseData, id, editExpense, singleExpense, warehouses, expenseCategories, frontSetting} = props;
+    const [isPaymentType,setIsPaymentType] = useState(false)
+
     const navigate = useNavigate();
     const [expenseValue, setExpenseValue] = useState({
         date: singleExpense ? moment(singleExpense[0].date).toDate() : new Date(),
@@ -20,6 +24,8 @@ const ExpenseForm = (props) => {
         amount: singleExpense ? singleExpense[0].amount : '',
         details: singleExpense ? singleExpense[0].details : '',
         title: singleExpense ? singleExpense[0].title : '',
+        payment_status: singleExpense?.payment_status ? singleExpense?.payment_status : '',
+        payment_type: {label: getFormattedMessage("payment-type.filter.cash.label"), value: 1}
     });
 
     const [errors, setErrors] = useState({
@@ -45,6 +51,10 @@ const ExpenseForm = (props) => {
             errorss['expense_category_id'] = getFormattedMessage('expense.input.expense-category.validate.label');
         } else if (!expenseValue['amount']) {
             errorss['amount'] = getFormattedMessage('expense.input.amount.validate.label');
+        } else if (!expenseValue?.payment_status) {
+            error['payment_status'] = getFormattedMessage("globally.payment.status.validate.label")
+        } else if (!expenseValue?.payment_type) {
+            error['payment_type'] = getFormattedMessage("globally.payment.type.validate.label")
         } else {
             isValid = true;
         }
@@ -68,6 +78,33 @@ const ExpenseForm = (props) => {
         setErrors('');
     };
 
+    
+    const onPaymentStatusChange = (obj) => {
+        setExpenseValue(inputs => ({...inputs, payment_status: obj}));
+        obj.value !== 2 ? setIsPaymentType(true) : setIsPaymentType(false)
+        setExpenseValue(input => ({...input, payment_type: {label: getFormattedMessage("payment-type.filter.cash.label"), value: 1}}))
+    };
+
+    const onPaymentTypeChange = (obj) => {
+        setExpenseValue(inputs => ({...inputs, payment_type: obj}));
+    };
+
+    const paymentStatusFilterOptions = getFormattedOptions(salePaymentStatusOptions)
+    const paymentStatusDefaultValue = paymentStatusFilterOptions.map((option) => {
+        return {
+            value: option.id,
+            label: option.name
+        }
+    })
+
+    const paymentMethodOption = getFormattedOptions(paymentMethodOptions)
+    const paymentTypeDefaultValue = paymentMethodOption.map((option) => {
+        return {
+            value: option.id,
+            label: option.name
+        }
+    })
+
     const handleCallback = (date) => {
         setExpenseValue(previousState => {
             return {...previousState, date: date}
@@ -82,6 +119,8 @@ const ExpenseForm = (props) => {
             expense_category_id: prepareData.expense_category_id.value,
             amount: prepareData.amount,
             details: prepareData.details,
+            payment_status: prepareData.payment_status.value ? prepareData.payment_status.value : prepareData.payment_status,
+            payment_type: prepareData.payment_status.value === 2 ? 0 : prepareData.payment_type.value ? prepareData.payment_type.value : prepareData.payment_type,
         }
         return formValue
     };
@@ -160,7 +199,27 @@ const ExpenseForm = (props) => {
                             <span
                                 className='text-danger d-block fw-400 fs-small mt-2'>{errors['amount'] ? errors['amount'] : null}</span>
                         </div>
-                        <div className='col-md-6 mb-3'>
+                         {/* ................... */}
+                         { !singleExpense && <div className='col-md-4'>
+                    <ReactSelect multiLanguageOption={paymentStatusFilterOptions} onChange={onPaymentStatusChange} name='payment_status'
+                         title={getFormattedMessage('dashboard.recentSales.paymentStatus.label')}
+                         value={expenseValue.payment_status} errors={errors['payment_status']}
+                         defaultValue={paymentStatusDefaultValue[0]}
+                         placeholder={placeholderText('sale.select.payment-status.placeholder')}/>
+                        </div>}
+                        { !singleExpense && expenseValue.payment_status.value !== 2 && <div className='col-md-4'>
+                            <ReactSelect title={getFormattedMessage('select.payment-type.label')}
+                            name='payment_type'
+                            value={expenseValue.payment_type} errors={errors['payment_type']}
+                            placeholder={placeholderText('sale.select.payment-type.placeholder')}
+                            defaultValue={paymentTypeDefaultValue[0]}
+                            multiLanguageOption={paymentMethodOption}
+                            onChange={onPaymentTypeChange}
+                        />
+                        </div>}
+                        
+                        {/* ... */}
+                        <div className='col-md-12 mb-3'>
                             <label
                                 className='form-label'>{getFormattedMessage('expense.input.details.label')}: </label>
                             <textarea name='details' className='form-control' rows='3'
